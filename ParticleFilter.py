ccbf92ee341948605d29ef3243a7036a82e34748
Jeff Herbst <jeffreyherbst18@gmail.com>
2018-04-11 09:25:33 -0600

    part 4 done and code cleanup

--- ParticleFilter.py
+++ ParticleFilter.py
@@ -56,21 +56,22 @@ class ParticleFilter:
 
     def sample_motion_model(self,time_delta):
         """ Simulates Gaussian noise and returns the move matrix for that noise
-            Used to update all of the particles
+            Called in generate
             Arguments:
                 time_delta: time elapsed since last update
             Returns:
                 T_motion transform matrix
         """
-        #current velocities
-        random_lin = np.random.normal(0,1)
-        random_ang = np.random.normal(0,1)
-
+        #random velocities based on alphas set in localization
+        random_lin = np.random.normal(self.alpha[0],self.alpha[1])
+        random_ang = np.random.normal(self.alpha[2],self.alpha[3])
         #print(random1, random2)
+
+        #add the randomness to our actual vleocity
         lin_vel = self.robot.lin_vel + random_lin
         ang_vel = self.robot.ang_vel + random_ang
 
-        #same as normal robot motion
+        #same as normal robot motion, just with the randomness added in
         if lin_vel == 0: #pure linear
             T_motion = transform(lin_vel * time_delta, 0, 0)
         elif ang_vel == 0: #pure rotational
@@ -80,36 +81,65 @@ class ParticleFilter:
             T_motion = transform(0, R, 0)
             T_motion = T_motion * transform(0, 0, ang_vel * time_delta)
             T_motion = T_motion * transform(0, -R, 0)
-
-        
+ 
         return T_motion
 
     def generate(self, time_delta):
-        """ Update partilces by sampling from teh motion model
+        """ Update partilces by sampling from the motion model
             Arguments:
-                time_delta: time elapsed since last updat
+                time_delta: time elapsed since last update
         """
-        #same simulated motion for all particles?, based on gaussian randomeness
-        T_motion = self.sample_motion_model(time_delta)
-        
-        #loop thru all particles
+         #loop thru all particles
         for x in range(0,self.num_particles - 1):
+            #get T_motion from above model
+            T_motion = self.sample_motion_model(time_delta)
+
             #get this particles position
             xPosition = self.particles[x,0]
             yPosition = self.particles[x,1]
+            theta = self.robot.theta
 
-            #create transform and move it, use robots direction
-            T_particle_map = transform(xPosition, yPosition, self.robot.theta)
-            T_particle_map = T_particle_map * T_motion 
+            #create transform and move it, use robots actual direction(no theta randomness)
+            T_robot_map = transform(xPosition, yPosition, theta)
+            T_robot_map = T_robot_map * T_motion 
 
-            #store particles new position back
-            self.particles[x,0] = T_particle_map[0,2]
-            self.particles[x,1] = T_particle_map[1,2]
+            #store particle's new position back
+            self.particles[x,0] = T_robot_map[0,2]
+            self.particles[x,1] = T_robot_map[1,2]
 
 
     def update(self):
         """ Update particle weights according to the rangefinder reading. """
-        pass
+        #total weight for normilization
+        total_weight = 0
+
+        for x in range(0, self.num_particles - 1):
+            #actual rangefinder reading
+            rangefinder_reading = self.robot.sonar_distance
+            
+            #create transform matricies
+            T_robot_map = transform(self.particles[x,0], self.particles[x,1], self.robot.theta)
+            T_sonar_robot = transform(0,0,self.robot.sonar_angle) * transform(self.robot.sonar_offset,0,0)
+            T_sonar_map = T_robot_map * T_sonar_robot
+
+            #constant for equation
+            sigma_squared = 10
+
+            #rangefinder reading with randomness
+            expected_rangefinder_reading = self.omap.get_first_hit(T_sonar_map, True) 
+            
+            #e^((-(rangefinder_reading - expected_rangefinder_reading)^2) / sigma_squared)
+            new_weight = math.exp( (-1 * math.pow((rangefinder_reading - expected_rangefinder_reading),2)) / sigma_squared )
+            
+            #save weight and update total
+            self.particle_weights[x] = new_weight
+            total_weight += new_weight
+            
+        #normalize
+        for x in range(0, self.num_particles - 1):
+            self.particle_weights[x] /= total_weight
+
+
 
     def sample(self):
         """ Re-sample particles according to their weights. """
